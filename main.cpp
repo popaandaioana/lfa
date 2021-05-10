f7ce48c88278c11bb75142d97a6ed447995df496
Popa Anda-Ioana <80072157+popaandaioana@users.noreply.github.com>
2021-05-10 19:23:10 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,115 +1,62 @@
 #include <iostream>
 #include <fstream>
-#include <string.h>
+#include <unordered_map>
+#include <vector>
 using namespace std;
-ifstream f("input.in");
+ifstream f("gram_reg.in");
+string tranz[10000]; //retin tranzitiile pe parcurs
+int n;
+unordered_map<string, vector<string>> mp; //retin input-ul
 
-//mat=matricea arcelor
-//nr_nod=numarul nodurilor
-//nr_tranz=numarul tranziilor
-//stare_in=starea initiala
-//nf=numar stari finale
-//stare_fin=vectorul starilor finale
-//ni=numarul cuvintelor
-//cuv=matricea cuvintelor
-
-struct arc //structura pentru memorarea tranzitiilor
-    {
-        int n1,n2;
-        char litera;
-    }mat[100];
-
-int nr_nod, nr_tranz,stare_in,nf,stare_fin[100],ni,stiva[100],vf,valid,varf;
-char cuv[100][100];
-
-void citire_fisier()//citeste automatul din fisier
+int nrterm(int y) //functie care calculeaza numarul de terminale
 {
-    int i;
-    f>>nr_nod>>nr_tranz;
-    for(i=0; i<=nr_tranz-1; i++)
-        f>>mat[i].n1>>mat[i].n2>>mat[i].litera;
-    f>>stare_in;
-    f>>nf;
-    for(i=0; i<=nf-1; i++)
-        f>>stare_fin[i];
-    f>>ni;
-    for(i=0; i<=ni-1; i++)
-        f>>cuv[i];
+    int nr=0;
+    for (int i=0; i<=y; i++)
+    {   if(tranz[i][0]!='N')
+            if(tranz[i]!="#")
+                nr++;
+    }
+    return nr;
 }
 
-int cauta(int nod, int v[], int l) //functia cauta un element intr-un vector de intregi de lungime l
-{
-    for(int i=0;i<l;i++)
-        if(nod==v[i])
-            return 1;
-    return 0;
+void afisterm(int m) //functie care afiseaza terminalele
+{   for (int i=0; i<=m; i++)
+        if(tranz[i][0]!='N' && tranz[i][0]!='#')
+            cout<<tranz[i][0]<<" ";
+        cout<<"\n";
 }
 
-void afn(int stare,char cuv[])
+void bkt(int k) //functie backtracking pentru gasirea solutiilor
 {
-//daca cuvantul nu mai are litere si starea se gaseste printre cele finale, atunci cuvantul e acceptat
-    if(strlen(cuv)==0 && cauta(stare,stare_fin,nf)==1)
-        {
-            valid=1;
-            stiva[vf]=stare;
-            vf++;
-            varf=vf;
-        }
-    else
-//daca cuvantul mai are litere, verific unde pot pleca din starea curenta cu prima litera din cuvant si apelez recursiv functia afn
-    if(strlen(cuv)>0 && valid==0)
- {
-            int ok=0;
-            for(int k=0;k<nr_tranz;k++)
+    if(tranz[k - 1][0]=='N' && nrterm(k-1)<= n) //cazul in care pe un nivel din tranz este un neterminal si nu am ajuns la nr de litere dorit
+            for(int i=0; i<mp[tranz[k-1]].size(); i++)
             {
-                if(mat[k].litera==cuv[0] && mat[k].n1==stare)
-                {
-                    ok=1;
-                    stiva[vf]=stare;
-                    vf++;
-                    int x=vf;
-                    afn(mat[k].n2,cuv+1);
-                    vf=x-1;
-                }
-
+                tranz[k]=mp[tranz[k-1]][i];
+                bkt(k+1);
             }
-            if(ok==0)
-                    vf--;
+        if(tranz[k-1].length()>=2 && nrterm(k-1)<=n) //cazul in care pe un nivel din tranz este o litera + un neterminal si
+                                                     //nu am ajuns la nr de litere dorit
+            if(tranz[k-1][1]=='N')
+            {
+                for(int i=0; i<mp[tranz[k-1].substr(1,2)].size(); i++)
+                {
+                    tranz[k]=mp[tranz[k-1].substr(1,2)][i];
+                    bkt(k+1);
+                }
+            }
+    if (nrterm(k-1)==n && tranz[k-1].find('N')==std::string::npos) //cazul in care am ajuns la numarul de litere dorit si nu este un neterminal
+            afisterm(k - 1); //afisam cuvantul
 }
-}
+
 int main()
 {
-   citire_fisier();//citeste automatul
-   for(int i=0; i<=ni-1; i++) //pentru fiecare cuvant, cuvantul curent=cuv[i]
-    {
-        valid=0;
-        for(int j=0;j<nr_tranz;j++)
-        {
-            vf=0;
-            if(mat[j].n1==stare_in && mat[j].litera==cuv[i][0]) //verifica unde poate sa plece din starea initiala cu prima litera din cuvant
-            {
-                stiva[vf]=stare_in;
-                vf++;
-                afn(mat[j].n2,cuv[i]+1); //pt fiecare nod gasit apeleaza functia afn(nod bun, cuvantul fara prima litera)
-            }
-        }
-
-        if(valid==1)
-            {
-            cout<<"DA"<<endl;
-            cout<<"Traseu: ";
-            for(int x=0;x<varf;x++)
-                cout<<stiva[x]<<" ";
-            cout<<endl;
-            }
-        else
-            cout<<"NU"<<endl;
-        if(strlen(cuv[i])==0 && cauta(stare_in,stare_fin,nf)==1)
-        {
-            valid=1;
-            cout<<"DA Traseu: "<<stare_in<<" ";
-        }
-    }
+    string a, b;
+    while (f>>a>>b) //citesc din fisier
+        mp[a].push_back(b); //si completez map-ul
+    cout<<"n= ";
+    cin>>n;
+    tranz[0]="NS"; //marchez in tranz punctul de plecare
+    bkt(1); //apelez functia de backtracking
     return 0;
 }
 
